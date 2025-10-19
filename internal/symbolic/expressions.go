ceac93f32596ea4f7398b3cb6795789b66399703
netos23 <nikmoroz2002@outlook.com>
2025-10-19 17:35:12 +0300

    feature(task2): add z3 translation

--- internal/symbolic/expressions.go
+++ internal/symbolic/expressions.go
@@ -1,7 +1,12 @@
 // Package symbolic содержит конкретные реализации символьных выражений
 package symbolic
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"symbolic-execution-course/internal/util"
+)
 
 // SymbolicExpression - базовый интерфейс для всех символьных выражений
 type SymbolicExpression interface {
@@ -16,32 +21,88 @@ type SymbolicExpression interface {
 }
 
 // SymbolicVariable представляет символьную переменную
-type SymbolicVariable struct {
-	Name     string
+type SymbolicVariable interface {
+	Name() string
+	Type() ExpressionType
+	TypeGeneric() *GenericType
+}
+
+type PrimitiveSymbolicVariable struct {
+	VarName  string
 	ExprType ExpressionType
 }
 
 // NewSymbolicVariable создаёт новую символьную переменную
-func NewSymbolicVariable(name string, exprType ExpressionType) *SymbolicVariable {
-	return &SymbolicVariable{
-		Name:     name,
+func NewSymbolicVariable(name string, exprType ExpressionType) *PrimitiveSymbolicVariable {
+	return &PrimitiveSymbolicVariable{
+		VarName:  name,
 		ExprType: exprType,
 	}
 }
 
+// Name возвращает имя переменной
+func (sv *PrimitiveSymbolicVariable) Name() string {
+	return sv.VarName
+}
+
 // Type возвращает тип переменной
-func (sv *SymbolicVariable) Type() ExpressionType {
+func (sv *PrimitiveSymbolicVariable) Type() ExpressionType {
 	return sv.ExprType
 }
 
 // String возвращает строковое представление переменной
-func (sv *SymbolicVariable) String() string {
-	return sv.Name
+func (sv *PrimitiveSymbolicVariable) String() string {
+	return sv.VarName
 }
 
 // Accept реализует Visitor pattern
-func (sv *SymbolicVariable) Accept(visitor Visitor) interface{} {
+func (sv *PrimitiveSymbolicVariable) Accept(visitor Visitor) interface{} {
 	return visitor.VisitVariable(sv)
+}
+
+// Accept реализует Visitor pattern
+func (sv *PrimitiveSymbolicVariable) TypeGeneric() *GenericType {
+	return nil
+}
+
+type ArraySymbolicVariable struct {
+	VarName  string
+	ExprType ExpressionType
+	Generic  GenericType
+}
+
+// NewSymbolicVariable создаёт новую символьную переменную
+func NewArraySymbolicVariable(name string, exprType ExpressionType, generic GenericType) *ArraySymbolicVariable {
+	return &ArraySymbolicVariable{
+		VarName:  name,
+		ExprType: exprType,
+		Generic:  generic,
+	}
+}
+
+// Name возвращает имя переменной
+func (sv *ArraySymbolicVariable) Name() string {
+	return sv.VarName
+}
+
+// Type возвращает тип переменной
+func (sv *ArraySymbolicVariable) Type() ExpressionType {
+	return sv.ExprType
+}
+
+// String возвращает строковое представление переменной
+func (sv *ArraySymbolicVariable) String() string {
+	return sv.VarName
+}
+
+// Accept реализует Visitor pattern
+func (sv *ArraySymbolicVariable) Accept(visitor Visitor) interface{} {
+	return visitor.VisitVariable(sv)
+}
+
+// Accept реализует Visitor pattern
+func (sv *ArraySymbolicVariable) TypeGeneric() *GenericType {
+	return &sv.Generic
 }
 
 // IntConstant представляет целочисленную константу
@@ -105,24 +166,75 @@ type BinaryOperation struct {
 
 // NewBinaryOperation создаёт новую бинарную операцию
 func NewBinaryOperation(left, right SymbolicExpression, op BinaryOperator) *BinaryOperation {
-	// TODO: Реализовать
-	// Создать новую бинарную операцию и проверить совместимость типов
-	panic("не реализовано")
+	if left.Type() != right.Type() {
+		return nil
+	}
+
+	switch op {
+	case ADD:
+		if left.Type() == BoolType {
+			return nil
+		}
+
+		if left.Type() == ArrayType {
+			return nil
+		}
+	case SUB:
+		if left.Type() == BoolType {
+			return nil
+		}
+
+		if left.Type() == ArrayType {
+			return nil
+		}
+	case MUL:
+		if left.Type() == BoolType {
+			return nil
+		}
+
+		if left.Type() == ArrayType {
+			return nil
+		}
+	case DIV:
+		if left.Type() == BoolType {
+			return nil
+		}
+
+		if left.Type() == ArrayType {
+			return nil
+		}
+	case MOD:
+		if left.Type() == BoolType {
+			return nil
+		}
+
+		if left.Type() == ArrayType {
+			return nil
+		}
+	}
+
+	return &BinaryOperation{
+		Operator: op,
+		Left:     left,
+		Right:    right,
+	}
 }
 
 // Type возвращает результирующий тип операции
 func (bo *BinaryOperation) Type() ExpressionType {
-	// TODO: Реализовать
-	// Определить результирующий тип на основе операции и типов операндов
-	// Например: int + int = int, int < int = bool
-	panic("не реализовано")
+	switch bo.Operator {
+	case ADD, SUB, MUL, DIV, MOD:
+		return bo.Left.Type()
+	case EQ, NE, LT, LE, GT, GE:
+		return BoolType
+	default:
+		panic("unknown type of binary operation")
+	}
 }
 
 // String возвращает строковое представление операции
 func (bo *BinaryOperation) String() string {
-	// TODO: Реализовать
-	// Формат: "(left operator right)"
-	panic("не реализовано")
+	return fmt.Sprintf("(%s %s %s)", bo.Left.String(), bo.Operator.String(), bo.Right.String())
 }
 
 // Accept реализует Visitor pattern
@@ -140,9 +252,39 @@ type LogicalOperation struct {
 
 // NewLogicalOperation создаёт новую логическую операцию
 func NewLogicalOperation(operands []SymbolicExpression, op LogicalOperator) *LogicalOperation {
-	// TODO: Реализовать
-	// Создать логическую операцию и проверить типы операндов
-	panic("не реализовано")
+	if slices.ContainsFunc(operands, func(e SymbolicExpression) bool {
+		return e.Type() != BoolType
+	}) {
+		return nil
+	}
+
+	switch op {
+	case NOT:
+		if len(operands) != 1 {
+			return nil
+		}
+
+		return &LogicalOperation{
+			Operands: operands,
+			Operator: op,
+		}
+	case OR, AND:
+		return &LogicalOperation{
+			Operands: operands,
+			Operator: op,
+		}
+	case IMPLIES:
+		if len(operands) != 2 {
+			return nil
+		}
+
+		return &LogicalOperation{
+			Operands: operands,
+			Operator: op,
+		}
+	}
+
+	return nil
 }
 
 // Type возвращает тип логической операции (всегда bool)
@@ -156,7 +298,26 @@ func (lo *LogicalOperation) String() string {
 	// Для NOT: "!operand"
 	// Для AND/OR: "(operand1 && operand2 && ...)"
 	// Для IMPLIES: "(operand1 => operand2)"
-	panic("не реализовано")
+	switch lo.Operator {
+	case NOT:
+		return fmt.Sprintf("!%s", lo.Operands[len(lo.Operands)-1].String())
+	case AND, OR:
+		return fmt.Sprintf(
+			"(%s)",
+			strings.Join(
+				util.Convert(
+					lo.Operands, func(e SymbolicExpression) string {
+						return e.String()
+					},
+				),
+				lo.Operator.String(),
+			),
+		)
+	case IMPLIES:
+		return fmt.Sprintf("(%s => %s)", lo.Operands[0].String(), lo.Operands[len(lo.Operands)-1].String())
+	default:
+		return ""
+	}
 }
 
 // Accept реализует Visitor pattern
@@ -240,8 +401,213 @@ func (op LogicalOperator) String() string {
 	}
 }
 
+type UnaryOperator int
+
+const (
+	PLUS UnaryOperator = iota
+	MINUS
+	CORRET
+)
+
+func (op UnaryOperator) String() string {
+	switch op {
+	case PLUS:
+		return "+"
+	case MINUS:
+		return "-"
+	case CORRET:
+		return "^"
+	default:
+		return "unknown"
+	}
+}
+
 // TODO: Добавьте дополнительные типы выражений по необходимости:
 // - UnaryOperation (унарные операции: -x, !x)
+
+type UnaryOperation struct {
+	Operand  SymbolicExpression
+	Operator UnaryOperator
+}
+
+// TODO: Реализуйте следующие методы в рамках домашнего задания
+
+// NewUnaryOperation создаёт новую бинарную операцию
+func NewUnaryOperation(operand SymbolicExpression, op UnaryOperator) *UnaryOperation {
+	if operand.Type() != IntType {
+		return nil
+	}
+
+	return &UnaryOperation{
+		Operand:  operand,
+		Operator: op,
+	}
+}
+
+// Type возвращает результирующий тип операции
+func (uo *UnaryOperation) Type() ExpressionType {
+	return uo.Operand.Type()
+}
+
+// String возвращает строковое представление операции
+func (uo *UnaryOperation) String() string {
+	return uo.Operand.String() + uo.Operator.String()
+}
+
+// Accept реализует Visitor pattern
+func (uo *UnaryOperation) Accept(visitor Visitor) interface{} {
+	return visitor.VisitUnaryOperation(uo)
+}
+
 // - ArrayAccess (доступ к элементам массива: arr[index])
+
+type ArraySelect struct {
+	Array ArraySymbolicVariable // Сам массив
+	Index SymbolicExpression    // Тип элемента массива
+}
+
+// NewArraySelect создаёт выражение arr[idx]
+func NewArraySelect(arr ArraySymbolicVariable, idx SymbolicExpression) *ArraySelect {
+	return &ArraySelect{Array: arr, Index: idx}
+}
+
+// Type возвращает тип элемента массива
+func (as *ArraySelect) Type() ExpressionType {
+	return as.Array.Generic.ExprType
+}
+
+// String:  arr[idx]
+func (as *ArraySelect) String() string {
+	return fmt.Sprintf("%s[%s]", as.Array.String(), as.Index.String())
+}
+
+// Accept реализует Visitor pattern
+func (as *ArraySelect) Accept(v Visitor) interface{} { return v.VisitArraySelect(as) }
+
 // - FunctionCall (вызовы функций: f(x, y))
+
+type Function struct {
+	Name       string
+	Args       []SymbolicVariable
+	ReturnType GenericType
+}
+
+// NewArraySelect создаёт выражение arr[idx]
+func NewFunction(name string, args []SymbolicVariable, returnType GenericType) *Function {
+	return &Function{
+		Name:       name,
+		Args:       args,
+		ReturnType: returnType,
+	}
+}
+
+// Type возвращает результирующий тип операции
+func (f *Function) Type() ExpressionType {
+	return f.ReturnType.ExprType
+}
+
+// String возвращает строковое представление операции
+func (f *Function) String() string {
+	return fmt.Sprintf(
+		"%s %s(%s)",
+		f.Type(),
+		f.Name,
+		strings.Join(
+			util.Convert(
+				f.Args, func(e SymbolicVariable) string {
+					return e.Name()
+				},
+			),
+			",",
+		),
+	)
+}
+
+// Accept реализует Visitor pattern
+func (f *Function) Accept(visitor Visitor) interface{} {
+	return visitor.VisitFunction(f)
+}
+
+type FunctionCall struct {
+	Func Function
+	Args []SymbolicExpression
+}
+
+// NewArraySelect создаёт выражение arr[idx]
+func NewFunctionCall(fun Function, args []SymbolicExpression) *FunctionCall {
+	for i, e := range args {
+		if e.Type() != args[i].Type() {
+			return nil
+		}
+	}
+
+	return &FunctionCall{
+		Func: fun,
+		Args: args,
+	}
+}
+
+// Type возвращает результирующий тип операции
+func (fc *FunctionCall) Type() ExpressionType {
+	return fc.Func.Type()
+}
+
+// String возвращает строковое представление операции
+func (fc *FunctionCall) String() string {
+	return fmt.Sprintf(
+		"%s(%s)",
+		fc.Func.Name,
+		strings.Join(
+			util.Convert(
+				fc.Args, func(e SymbolicExpression) string {
+					return e.String()
+				},
+			),
+			",",
+		),
+	)
+}
+
+// Accept реализует Visitor pattern
+func (fc *FunctionCall) Accept(visitor Visitor) interface{} {
+	return visitor.VisitFunctionCall(fc)
+}
+
 // - ConditionalExpression (тернарный оператор: condition ? true_expr : false_expr)
+
+type ConditionalExpression struct {
+	Condition LogicalOperation
+	Then      SymbolicExpression
+	Else      SymbolicExpression
+}
+
+func NewConditionalExpression(
+	condition LogicalOperation,
+	then SymbolicExpression,
+	elze SymbolicExpression,
+) *ConditionalExpression {
+	if then.Type() != elze.Type() {
+		return nil
+	}
+
+	return &ConditionalExpression{
+		Condition: condition,
+		Then:      then,
+		Else:      elze,
+	}
+}
+
+// Type возвращает результирующий тип операции
+func (ce *ConditionalExpression) Type() ExpressionType {
+	return ce.Then.Type()
+}
+
+// String возвращает строковое представление операции
+func (ce *ConditionalExpression) String() string {
+	return fmt.Sprintf("(%s ? %s : %s)", ce.Condition.String(), ce.Then.String(), ce.Else.String())
+}
+
+// Accept реализует Visitor pattern
+func (ce *ConditionalExpression) Accept(visitor Visitor) interface{} {
+	return visitor.VisitConditionalExpression(ce)
+}
--- internal/symbolic/types.go
+++ internal/symbolic/types.go
@@ -1,6 +1,8 @@
 // Package symbolic определяет базовые типы символьных выражений
 package symbolic
 
+import "fmt"
+
 // ExpressionType представляет тип символьного выражения
 type ExpressionType int
 
@@ -10,6 +12,19 @@ const (
 	ArrayType
 	// Добавьте другие типы по необходимости
 )
+
+type GenericType struct {
+	ExprType ExpressionType
+	Generic  *GenericType
+}
+
+func (g *GenericType) String() string {
+	if g.Generic == nil {
+		return g.Generic.ExprType.String()
+	}
+
+	return fmt.Sprintf("%s[%s]", g.ExprType.String(), g.Generic.String())
+}
 
 // String возвращает строковое представление типа
 func (et ExpressionType) String() string {
